5ee87fe8295c196dd67721070c31c7780225add7
asheravraham <asheravraham@gmail.com>
2018-08-27 22:22:58 +0300

    Updated project structure

--- src/main/java/pages/LoginPage.java
+++ src/main/java/pages/LoginPage.java
@@ -1,5 +1,27 @@
 package pages;
 
-public class LoginPage {
+import Utils.Base;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.How;
+
+public class LoginPage extends Base{
+
+
+    @FindBy(how = How.CSS, using = "#menu-item-17422")
+    public WebElement createbutton;
+
+    @FindBy(how = How.CSS, using = "#joinUserName")
+    public WebElement signUpEmail;
+
+    @FindBy(how = How.XPATH, using = "//*[@id=\"registerForm\"]/div[2]/div/div/input")
+    public WebElement signUpPassword;
+
+    @FindBy(how = How.XPATH, using = "//*[@id=\"repassword\"]")
+    public WebElement rePassword;
+
+    @FindBy(how = How.XPATH, using = "//*[@id=\"registerForm\"]/div[4]/div/button")
+    public WebElement joinButton;
+
 
 }
